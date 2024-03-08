c4e4ad2766c4d0cc11ea7065065fd3fb14490c8f
Laxr24 <73450829+Laxr24@users.noreply.github.com>
2024-03-08 11:20:54 -0500

    added dashboard page

--- API-server/frontend/assets/js/app.js
+++ API-server/frontend/assets/js/app.js
@@ -6,9 +6,16 @@ const logInBtn = document.getElementById("loginBtn")
 
 
 loginBtn.onclick = ()=>{
+    let _uName, _uPass; 
+
     if(uName.value != "" && uPass.value != ""){
-        alert(uName.value + " " + uPass.value); 
+        _uName = uName.value; 
+        _uPass = uPass.value; 
     }else{
-        alert("Nothing there")
+        alert("You are not authorized :( ")
     }
+
+    if(_uName == 'shaan' && _uPass == 'shan'){
+        window.location = 'dashboard.html'
+}
 }
